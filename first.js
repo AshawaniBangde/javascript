0622a94e50f1a1d2a1d9ad512d0dd7875572a034
geeta <bangdegeeta@gmail.com>
2025-02-27 19:49:13 +0530

    javascript

---
+++ first.js
@@ -0,0 +1,123 @@
+
+
+// name  = "geeta";
+// age = 24;
+// price = 13.34;
+// x = null
+// y = undefined;
+// radious = 14
+// isFollow = false;
+
+// fullname ="geeta";
+// FULLNAME = "GEETA";
+// console.log(fullname);
+// console.log(FULLNAME);
+
+
+// let name = 'geeta';
+// let age =24;
+// let totalPrice = 800;
+
+
+// var myFavNum = "vinod";
+// console.log(myFavNum);
+
+// var myName = 'vinod';
+// if(myName){
+//     console.log('it is reaning truthhly b')
+// }else {
+//     console.log('its is false value')
+// }
+
+
+// console.log(parseInt('123'));
+
+// console.log(parseInt(123,10));
+
+// console.log(parseInt ("               123 "));
+
+
+// console.log(parseInt("077"));
+// console.log(parseFloat("077"));
+// console.log(parseInt("1.9"));
+// console.log(parseFloat("1.9"));
+// Nan === Nan 
+// if (Nan === Nan){
+//     console.log('both are equal')
+// }else{
+//     console.log("not equal")
+// }
+
+
+let sum = "vinod" + 5;
+console.log(sum); //output vinod5
+
+let add = "5" + 5;
+console.log(add); //output 0
+
+let ad = "5" - "5";
+console.log(ad); //output 0
+console.log(10 + "20") //output 1020
+console.log("java" + "script"); // output javascript
+console.log(" " + " ")  //output blank
+console.log(" " + 0); //output 0
+console.log("vinod" - "thapa"); //NaN
+console.log(true + true); // 2
+console.log(true + false) //1
+console.log(false + true) //1
+console.log(false + false) //0
+console.log(5===5) //true
+console.log(5==="5") //false
+console.log(5!==5); //false
+console.log(5!=="5"); //true
+console.log(5>2) //true
+console.log(5<2) //false
+console.log(10<=10) //true
+
+var a = 5;
+var b = 6;
+console.log(a < 0 && b < 0); //false
+
+var a = 10;
+var b = 20;
+console.log(a>10 || b>20); // false
+
+var isOpen = false;
+console.log(!isOpen); //true
+
+// Interview question
+var age = 18;
+var hadDrrivingLicence = true;
+
+if(age >= 18){
+    console.log('haddriving line true');
+
+}else{
+    console.log('hadnotdrivinglince false')
+}
+
+
+
+let multiage = 19;
+let result = multiage >= 18 ? 'yes' : 'no'
+console.log(result);
+
+console.log(2 < 12 < 5); //true left to right
+
+console.log("20" + 10 + 10); //201010 
+console.log("5" - 3)
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
